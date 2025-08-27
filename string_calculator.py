ddacc18386392df6d4d2f9dbe9e4b678b5a8185a
SCKA-1482 <80355270+SCKA-1482@users.noreply.github.com>
2025-08-27 14:33:45 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -2,35 +2,34 @@ import re
 
 class StringCalculator:
     def add(self, numbers: str) -> int:
-        if numbers == "":
-            return 0
+        delimiter, numbers_part = self._extract_delimiter(numbers)
+        number_list = self._split_numbers(numbers_part, delimiter)
+        self._check_negatives(number_list)
+        filtered_numbers = self._filter_large_numbers(number_list)
+        return sum(filtered_numbers)
 
-        delimiters = [",", "\n"]
-        nums_str = numbers
-
+    def _extract_delimiter(self, numbers: str):
+        default_delimiters = [',', '\n']
         if numbers.startswith("//"):
-            delimiter_part, nums_str = numbers.split("\n", 1)
-            custom_delimiters = re.findall(r"\[(.*?)\]", delimiter_part)
-            if custom_delimiters:
-                delimiters = custom_delimiters
+            delimiter_part, numbers_part = numbers[2:].split('\n', 1)
+            if delimiter_part.startswith("[") and delimiter_part.endswith("]"):
+                delimiter = delimiter_part[1:-1]
             else:
-                delimiters = [delimiter_part[2:]]
+                delimiter = delimiter_part
+            return [delimiter], numbers_part
+        else:
+            return default_delimiters, numbers
 
-        delimiter_pattern = "|".join(map(re.escape, delimiters))
-        parts = re.split(delimiter_pattern, nums_str)
+    def _split_numbers(self, numbers_part: str, delimiters: list[str]):
+        delimiters_pattern = '|'.join(re.escape(d) for d in delimiters)
+        tokens = re.split(delimiters_pattern, numbers_part)
+        return [int(token) for token in tokens if token != '']
 
-        negatives = []
-        total = 0
-        for part in parts:
-            if part == "":
-                continue
-            num = int(part)
-            if num < 0:
-                negatives.append(num)
-            elif num <= 1000:
-                total += num
-
+    def _check_negatives(self, numbers: list[int]):
+        negatives = [n for n in numbers if n < 0]
         if negatives:
-            raise ValueError("negatives not allowed: " + ", ".join(str(n) for n in negatives))
+            negatives_str = ", ".join(str(n) for n in negatives)
+            raise ValueError(f"negatives not allowed: {negatives_str}")
 
-        return total
+    def _filter_large_numbers(self, numbers: list[int]) -> list[int]:
+        return [n for n in numbers if n <= 1000]
